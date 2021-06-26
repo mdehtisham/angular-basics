c831abb7b8f7493dd7c395bd0a615f24b1063fff
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-06-26 08:51:54 +0530

    checked ngIf with 3 conditions

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
+  first = true;
+  second = false;
+  third = false;
 
   constructor() { }
 
