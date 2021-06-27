4cd57587d8400cdc5503366b03c22967e2700ba3
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-06-27 23:02:39 +0530

    passed data from paret to child component

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  posts = [
+    { imageUrl: "assets/cat.jpg", title: "cat", content: "cat is a domestic animal" },
+    { imageUrl: "assets/dog.jpg", title: "dog", content: "dog is also a domestic animal" },
+    { imageUrl: "assets/dog.jpg", title: "dog", content: "dog is also a domestic animal" },
+    { imageUrl: "assets/dog.jpg", title: "dog", content: "dog is also a domestic animal" },
+    { imageUrl: "assets/dog.jpg", title: "dog", content: "dog is also a domestic animal" },
+    { imageUrl: "assets/mouse.jpg", title: "mouse", content: "mouse is not a domestic animal" },
+  ]
 
   constructor() { }
 
